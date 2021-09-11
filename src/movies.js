9afdf6eae97977ab3ed37ffdf84e403ce353736f
evanina <eva.degano@gmail.com>
2021-09-11 16:52:40 +0200

    iteration 4

--- src/movies.js
+++ src/movies.js
@@ -9,23 +9,23 @@ function getAllDirectors (movies) {
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies (movies) {
-  let spielbergDramaMovies = movies.filter((movie) => movie.director == "Steven Spielberg" && movie.genre.indexOf("Drama") != -1);
+  let spielbergDramaMovies = movies.filter((movie) => movie.director === "Steven Spielberg" && movie.genre.indexOf("Drama") != -1);
   return spielbergDramaMovies.length;
 }
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage (movies) {
-  /*records.filter(({gender}) => gender === 'BOYS')
-  .reduce((sum, record) => sum + record.value)*/
-  
-  let average =  movies.filter((movie) => !!movie.score).reduce(function(acc, movie){
-      return acc + movie.score/movies.length;
+function scoresAverage(movies) {
+  let average =  movies.filter((movie) => !!movie.score).reduce(function(acc, movie) {
+    return acc + movie.score / movies.length;
   }, 0);
   return Number(average.toFixed(2));
 }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(movies) {
+  let dramaMovies = movies.filter((movie) => movie.genre.indexOf("Drama") != -1);
+  return scoresAverage(dramaMovies);
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear() {}
