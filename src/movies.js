a0ed186e5b872e8aaa974815b6c580d08d886733
aude.claquesin <aude.claquesin@gmail.com>
2021-09-12 12:05:25 +0200

    Iterations 1 to 6 completed

--- src/movies.js
+++ src/movies.js
@@ -1,3 +1,5 @@
+let movies = require("./data");
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
@@ -8,24 +10,25 @@ function getAllDirectors(films) {
   return directors
 };
 
-console.log(getAllDirectors(movies))
+//console.log(getAllDirectors(movies))
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(films) {
   const spielbergMovies = films.filter(function (moviesSpielbergDrama){
+    if (films.length<1) return 0; // in case of empty array
     return moviesSpielbergDrama.director==='Steven Spielberg' && moviesSpielbergDrama.genre.includes(`Drama`);
   })
-  return spielbergMovies
+  return Number(spielbergMovies.length) // transform object to number
 };
 
-console.log(howManyMovies(movies))
+//console.log(howManyMovies(movies))
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesScore) {
   const avgScores = moviesScore.reduce(function(avg, score2) {
     return avg + (score2.score)/moviesScore.length
   },0)
-  return avgScores.toFixed(2)                                  
+  return Number(avgScores.toFixed(2)) || 0                              
 };
 
 console.log(scoresAverage(movies))
@@ -38,7 +41,7 @@ function dramaMoviesScore(bestDrama) {
   const avgDramaScore = onlyDrama.reduce(function(avg, score2) {
     return avg + (score2.score)/onlyDrama.length
   },0);
-  return avgDramaScore.toFixed(2)  
+  return Number(avgDramaScore.toFixed(2))
 };
 
 console.log(dramaMoviesScore(movies))
@@ -46,7 +49,10 @@ console.log(dramaMoviesScore(movies))
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesReleaseDate) {
   const movieYear = moviesReleaseDate.sort(function (a, b) {
-  return a.year - b.year
+  if (a.year === b.year) {
+    return a.title - b.title ; 
+  }
+    return a.year - b.year
   });
   return movieYear
 };
@@ -64,7 +70,10 @@ function orderAlphabetically(moviesTitle) {
     }
     return 0;
     });
-  return alphaOrder.slice(0,20)
+  const alphaOrderTitle = alphaOrder.map(function (onlyTitles){
+    return onlyTitles.title
+  });
+  return alphaOrderTitle.slice(0,20)
 };
 
 console.log(orderAlphabetically(movies))
