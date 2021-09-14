d9ec046d71c82af1024fa4a8b26a4c3bba269f16
evanina <eva.degano@gmail.com>
2021-09-14 19:17:52 +0200

    iteration6

--- src/movies.js
+++ src/movies.js
@@ -31,7 +31,11 @@ function dramaMoviesScore(movies) {
 function orderByYear() {}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically(movies) {
+  let sortedArray = movies;
+  
+  return sortedArray.map((movie) => movie.title).sort().slice(0, 20);
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
